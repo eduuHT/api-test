eaae946f39e5d0b68fc459758335863d2007747f
eduuHT <eduuu.ht@gmail.com>
2025-11-26 12:37:32 -0700

    Add Models

---
+++ api_test/Models/Author.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace api_test.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    public List<Book>? Books { get; set; }
+}
---
+++ api_test/Models/Book.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace api_test.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public int Pages { get; set; }
+    public string Title { get; set; } = string.Empty;
+
+    public required Author Author { get; set; }
+}
